6dfe418d720e0761800ce6fbb4a565c2a9222fb4
Gustavo de Medeiros Carlos <gustavo.10.dmc@gmail.com>
2019-11-14 15:02:05 -0300

    Adiciona endGame

--- tick.c
+++ tick.c
@@ -7,6 +7,9 @@
 #include "fisica.h"
 
 static long int tick = 0;
+int loser = 0;
+int endTick = -1;
+
 WINDOW *w;
 PIC rasc;
 PIC fundo;
@@ -15,9 +18,20 @@ int getTick() {
     return tick;
 }
 
+void endGame() {
+    endTick = tick + 5 * TICKS_PER_FRAME * FRAMES_PER_SECOND;
+    if (loser == 1) {
+        printf("Jogador 2 ganhou!\n");
+    } else if (loser == 2) {
+        printf("Jogador 1 ganhou!\n");
+    } else {
+        printf("Ambos perderam!\n");
+    }
+}
+
 void gameLoop() {
     tick = 0;
-    while (1) {
+    while (endTick == -1 || tick <= endTick) {
         atualizaPosicoes();
         detectaColisoes();
         mataObjetos();
@@ -25,6 +39,11 @@ void gameLoop() {
             PutPic(rasc, fundo, 0, 0, WIDTH, HEIGHT, 0, 0);
             imprimaObjetos(rasc);
             PutPic(w, rasc, 0, 0, WIDTH, HEIGHT, 0, 0);
+        }
+        if (endTick == -1) {
+            loser += nave[0].obj == NULL ? 1 : 0;
+            loser += nave[1].obj == NULL ? 2 : 0;
+            if (loser != 0) endGame();
         }
         usleep(1000000.0 * passoSimulacao);
         tick++;
