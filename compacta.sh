1286ac5e88218ed50ed99bbb28109bbde6789720
Gustavo de Medeiros Carlos <gustavo.10.dmc@gmail.com>
2019-11-17 20:34:12 -0300

    Adiciona install-sh

--- compacta.sh
+++ compacta.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar -cvf ep3.tar assets/ aclocal.m4 AUTHORS ChangeLog compile config.h.in configure configure.ac COPYING depcomp *.h *.c Makefile.am Makefile.in missing NEWS README build.sh config.txt LEIAOREADME
+tar -cvf ep3.tar assets/ aclocal.m4 AUTHORS ChangeLog compile config.h.in configure configure.ac COPYING depcomp *.h *.c Makefile.am Makefile.in missing NEWS README build.sh config.txt LEIAOREADME install-sh
