0fb57b4919282d4fa06669d0ca369969ae43c678
Gustavo de Medeiros Carlos <gustavo.10.dmc@gmail.com>
2019-12-04 14:08:57 -0300

    Grandes ajustes

--- compacta.sh
+++ compacta.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar -cvf ep3.tar assets/ aclocal.m4 AUTHORS ChangeLog compile config.h.in configure configure.ac COPYING depcomp *.h *.c Makefile.am Makefile.in missing NEWS README build.sh config.txt LEIAOREADME install-sh
+tar -cvf ep4.tar assets/ aclocal.m4 AUTHORS ChangeLog compile config.h.in configure configure.ac COPYING depcomp *.h *.c Makefile.am Makefile.in missing NEWS README build.sh config.txt LEIAOREADME install-sh
